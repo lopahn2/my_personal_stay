770e08df6024ce80c6ebb4a7e8b67c74cbda32e7
kimjunha <kimjunha1575@gmail.com>
2025-02-27 16:53:32 +0900

    feat: Add repositories

--- src/main/java/com/spring/mypersonalstay/repository/GuestHouseRepository.java
+++ src/main/java/com/spring/mypersonalstay/repository/GuestHouseRepository.java
@@ -14,6 +14,8 @@ public interface GuestHouseRepository extends JpaRepository<GuestHouse, Long> {
 	List<GuestHouse> findByTagsContains(String tag);
 
 	List<GuestHouse> findByCapacityBetween(Integer min, Integer max);
+	
+	Optional<GuestHouse> findById(Long guestHouseid);
 
 	// 모든 게스트 하우스 점수 보여주기
 	List<GuestHouse> getAllGuestHousesScore();
