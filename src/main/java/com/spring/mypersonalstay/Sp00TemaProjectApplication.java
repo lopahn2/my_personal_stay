ef1750a254e9b6f1b5c0fdaaeb87b7f022316b7c
lopahn2 <lopahn2@gmail.com>
2025-02-27 14:06:58 +0900

    init: initiate spring project

---
+++ src/main/java/com/spring/mypersonalstay/Sp00TemaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.spring.mypersonalstay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp00TemaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sp00TemaProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/mypersonalstay/Sp00TemaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.mypersonalstay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sp00TemaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
