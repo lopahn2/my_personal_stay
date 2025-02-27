460a1f5179ef8aa52b71157c0567910f2821c018
lopahn2 <lopahn2@gmail.com>
2025-02-27 15:36:09 +0900

    feat: Mapping Member to Status and Status to GuestHouse

--- src/main/java/com/spring/mypersonalstay/entity/Status.java
+++ src/main/java/com/spring/mypersonalstay/entity/Status.java
@@ -6,9 +6,12 @@ import org.hibernate.annotations.CreationTimestamp;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -34,6 +37,18 @@ public class Status {
 	@CreationTimestamp
 	private LocalDateTime regDate;
 	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id")
 	private Member member;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "guest_house_id")
 	private GuestHouse guestHouse;
+
+
+	@Override
+	public String toString() {
+		return "Status [statusId=" + statusId + ", isLiked=" + isLiked + ", isBooked=" + isBooked + ", isUsed=" + isUsed
+				+ ", regDate=" + regDate + "]";
+	}
 }
