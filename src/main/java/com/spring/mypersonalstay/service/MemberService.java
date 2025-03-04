429b6c56d55d713fca2748bbaa19406692009d40
unknown <kjyeon1101@gmail.com>
2025-03-04 15:59:27 +0900

    [feat] add MemberController

--- src/main/java/com/spring/mypersonalstay/service/MemberService.java
+++ src/main/java/com/spring/mypersonalstay/service/MemberService.java
@@ -1,11 +1,13 @@
 package com.spring.mypersonalstay.service;
 
 import java.util.Date;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.spring.mypersonalstay.dto.member.MemberResDto;
 import com.spring.mypersonalstay.dto.member.ReqSignInDto;
 import com.spring.mypersonalstay.entity.Member;
 import com.spring.mypersonalstay.handler.CustomException;
@@ -30,5 +32,14 @@ public class MemberService {
 				.withIssuedAt(new Date())
 				.withExpiresAt(new Date(System.currentTimeMillis() + EXPIRED_TIME))
 				.sign(Algorithm.HMAC256(SECRET_KEY));
+	}
+	
+	public MemberResDto getMember(String jwtToken) {
+		String memberId = JWT.decode(jwtToken).getSubject();
+		Optional<Member> member = memberRepository.findById(Long.valueOf(memberId));
+		if (member == null) {
+			throw new CustomException(StatusCode.NOT_FOUND);
+		}
+		return new MemberResDto(member.get());
 	}
 }
