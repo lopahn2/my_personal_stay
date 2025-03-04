f5e7262955b76635208c47ed156cc312c73d1e23
lopahn2 <lopahn2@gmail.com>
2025-03-04 16:33:58 +0900

    fix: change exception construct Exception to CustomException

--- src/main/java/com/spring/mypersonalstay/service/ScoreService.java
+++ src/main/java/com/spring/mypersonalstay/service/ScoreService.java
@@ -28,66 +28,49 @@ public class ScoreService {
 	private final ScoreRepository scoreRepository;
 	private final GuestHouseRepository guestHouseRepository;
 	private final StatusRepository statusRepository;
-	
-	
+
 	public List<String> parseTags(String tags) {
 		return Arrays.asList(tags.split(","));
 	}
-	
+
 	public Integer getScoreWithTags(List<String> tags, String mbti) {
 		return tags.stream().filter((t) -> mbti.equals(t)).toList().size();
 	}
-	
-	public void run() {
-		scoreRepository.findAll().forEach(s -> System.out.println(s));
-	}
-	
+
 	@Transactional
-	public ScoreRes findScore(Long id) throws Exception{
-		Score rScore = scoreRepository.findById(id)
-				.orElseThrow(()->
-				new CustomException(StatusCode.NOT_FOUND)); 
-		
-		return new ScoreRes(rScore); 
-	}
-	
-	@Transactional
-	public ScoreRes updateScore(ScoreReq scoreReq) throws Exception {
-		Score rScore = (Score) guestHouseRepository.findById(scoreReq.getGuestHouseId())
-				.get()
-				.getScores()
-				.stream()
-				.filter(s -> s.getMbti().equals(scoreReq.getMbti()));
-		
-		GuestHouse guestHouse = guestHouseRepository.findById(scoreReq.getGuestHouseId()).get();
-		List<Status> rStatusList = statusRepository.findByGuestHouse(guestHouse);
-		int tmpScore = rStatusList.stream().filter((t) -> 
-				t.getMember()
-				.getMbti()
-				.equals(rScore.getMbti()))
-			.toList()
-			.size();
-		rScore.setTotalScore(rScore.getTotalScore()+tmpScore);
-		
-		return new ScoreRes(rScore);
-	}
-	
-	@Transactional
-	public ScoreRes initScore(ScoreReq scoreReq) throws Exception {
-		Score rScore = (Score) guestHouseRepository.findById(scoreReq.getGuestHouseId())
-			.get()
-			.getScores()
-			.stream()
-			.filter(s -> s.getMbti().equals(scoreReq.getMbti()));
-			
-		String mbti = guestHouseRepository
-				.findById(scoreReq.getGuestHouseId())
-				.get()
-				.getTags();
-		rScore.setTotalScore((float)getScoreWithTags(parseTags(mbti), rScore.getMbti()));
-		
+	public ScoreRes findScore(Long id) {
+		Score rScore = scoreRepository.findById(id).orElseThrow(() -> new CustomException(StatusCode.NOT_FOUND));
+
 		return new ScoreRes(rScore);
 	}
 
+	@Transactional
+	public ScoreRes updateScore(ScoreReq scoreReq) {
+		Score rScore = (Score) guestHouseRepository.findById(scoreReq.getGuestHouseId())
+				.orElseThrow(() -> new CustomException(StatusCode.NOT_FOUND)).getScores().stream()
+				.filter(s -> s.getMbti().equals(scoreReq.getMbti()));
+
+		GuestHouse guestHouse = guestHouseRepository.findById(scoreReq.getGuestHouseId())
+				.orElseThrow(() -> new CustomException(StatusCode.NOT_FOUND));
+		List<Status> rStatusList = statusRepository.findByGuestHouse(guestHouse);
+		int tmpScore = rStatusList.stream().filter((t) -> t.getMember().getMbti().equals(rScore.getMbti())).toList()
+				.size();
+		rScore.setTotalScore(rScore.getTotalScore() + tmpScore);
+
+		return new ScoreRes(rScore);
+	}
+
+	@Transactional
+	public ScoreRes initScore(ScoreReq scoreReq) {
+		Score rScore = (Score) guestHouseRepository.findById(scoreReq.getGuestHouseId())
+				.orElseThrow(() -> new CustomException(StatusCode.NOT_FOUND)).getScores().stream()
+				.filter(s -> s.getMbti().equals(scoreReq.getMbti()));
+
+		String mbti = guestHouseRepository.findById(scoreReq.getGuestHouseId())
+				.orElseThrow(() -> new CustomException(StatusCode.NOT_FOUND)).getTags();
+		rScore.setTotalScore((float) getScoreWithTags(parseTags(mbti), rScore.getMbti()));
+
+		return new ScoreRes(rScore);
+	}
 
 }
