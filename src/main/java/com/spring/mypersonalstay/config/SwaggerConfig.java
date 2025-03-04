0cc4349504ef766497d4d8b6f7d336e6498abdd7
unknown <kjyeon1101@gmail.com>
2025-03-04 17:23:32 +0900

    [feat] add swagger

---
+++ src/main/java/com/spring/mypersonalstay/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.spring.mypersonalstay.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+
+@Configuration
+public class SwaggerConfig {
+	@Bean
+	public OpenAPI openAPI() {
+		return new OpenAPI()
+				.components(new Components())
+				.info(apiInfo());
+	}
+	
+	private Info apiInfo() {
+		return new Info()
+				.title("REST API Specifications")
+				.description("My Personal Stay")
+				.version("1.0.0");
+	}
+}
