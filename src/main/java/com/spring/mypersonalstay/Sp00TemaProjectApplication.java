74179cb78ab24b18b28a1696052561e864638157
Pigeonine <hyen1222@gmail.com>
2025-03-06 10:12:02 +0900

    init and update command for score added

--- src/main/java/com/spring/mypersonalstay/Sp00TemaProjectApplication.java
+++ src/main/java/com/spring/mypersonalstay/Sp00TemaProjectApplication.java
@@ -1,22 +1,48 @@
 package com.spring.mypersonalstay;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 import com.spring.mypersonalstay.dto.ScoreReq;
-import com.spring.mypersonalstay.dto.ScoreRes;
-import com.spring.mypersonalstay.dto.member.ReqSignInDto;
-import com.spring.mypersonalstay.exception.CustomException;
-import com.spring.mypersonalstay.service.MemberService;
 import com.spring.mypersonalstay.service.ScoreService;
 
 @SpringBootApplication
 public class Sp00TemaProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(Sp00TemaProjectApplication.class, args);
+		
 	}
-
+	//초기 Score값 초기화
+//	@Bean
+//    public CommandLineRunner initScores(ScoreService scoreService) {
+//        return args -> {
+//            // 16가지 MBTI 유형 배열
+//            String[] mbtiTypes = {
+//                "INTP", "INTJ", "INFP", "INFJ",
+//                "ISTP", "ISTJ", "ISFP", "ISFJ",
+//                "ENTP", "ENTJ", "ENFP", "ENFJ",
+//                "ESTP", "ESTJ", "ESFP", "ESFJ"
+//            };
+//
+//            // guestHouseId가 1부터 20까지
+//            for (long guestHouseId = 1; guestHouseId <= 20; guestHouseId++) {
+//                for (String mbti : mbtiTypes) {
+//                    ScoreReq scoreReq = new ScoreReq(mbti, guestHouseId);
+//                    try {
+//                        // score 등록 (initScore 호출)
+//                        scoreService.initScore(scoreReq);
+//                        // score 업데이트 (updateScore 호출)
+//                        scoreService.updateScore(scoreReq);
+//                        System.out.println("Success for guestHouseId: " + guestHouseId + ", MBTI: " + mbti);
+//                    } catch (Exception e) {
+//                        System.out.println("Failed for guestHouseId: " + guestHouseId + ", MBTI: " + mbti 
+//                                + " - " + e.getMessage());
+//                    }
+//                }
+//            }
+//        };
+//	}
 }
